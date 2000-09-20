eb6989bc3dc5fd08e0e1f2ab7c2efe17da47eac6
Dave Love <fx@gnu.org>
2000-09-20 09:56:31 +0000

    (gnus-version-number): Start 5.9 series.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -258,7 +258,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "5.8.8"
+(defconst gnus-version-number "5.9.0"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "Gnus v%s" gnus-version-number)
