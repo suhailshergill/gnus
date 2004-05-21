3c11144e5b2e7ed87c5936a9fe6e1c86151cb33d
Miles Bader <miles@gnu.org>
2004-05-21 00:18:41 +0000

    Revision: miles@gnu.org--gnu-2004/gnus--rel--5.10--patch-6
    Merge from gnus--rel--5.8
    
    Patches applied:
    
     * miles@gnu.org--gnu-2004/gnus--rel--5.8--patch-3
       Remove RCS keywords

--- contrib/vcard.el
+++ contrib/vcard.el
@@ -7,8 +7,6 @@
 ;; Keywords: extensions
 ;; Created: 1997-09-27
 
-;; $Id: vcard.el,v 6.1 2001/07/10 21:35:06 zsh Exp $
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
