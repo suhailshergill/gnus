dc1143866173eb93a97147d9fb4461ce54ddb605
Reiner Steib <Reiner.Steib@gmx.de>
2007-11-04 21:37:44 +0000

    Bump version to 5.10.9.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -289,7 +289,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "5.10.8"
+(defconst gnus-version-number "5.10.9"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "Gnus v%s" gnus-version-number)
