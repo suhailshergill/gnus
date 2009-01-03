a9196279dd080e5c2036673c07dc309cb4a762be
Reiner Steib <Reiner.Steib@gmx.de>
2009-01-03 12:49:46 +0000

    Gnus v5.10.12 is released.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -289,7 +289,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "5.10.11"
+(defconst gnus-version-number "5.10.12"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "Gnus v%s" gnus-version-number)
