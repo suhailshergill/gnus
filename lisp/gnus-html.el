93112af86b3e0d6c1c2afe6a21b4ae9b9855df20
Lars Magne Ingebrigtsen <larsi@quimbies.gnus.org>
2010-08-29 04:40:49 +0200

    Allow pre-fetching images asynchronously.

--- lisp/gnus-html.el
+++ lisp/gnus-html.el
@@ -162,4 +162,20 @@
 	  (decf total-size (cadr file))
 	  (delete-file (nth 2 file)))))))
 
+(defun gnus-html-prefetch-images ()
+  (save-match-data
+    (let (urls)
+      (while (re-search-forward "<img.*src=\"\\([^\"]+\\)" nil t)
+	(let ((url (match-string 1)))
+	  (when (or (null mm-w3m-safe-url-regexp)
+		    (string-match mm-w3m-safe-url-regexp url))
+	    (unless (file-exists-p (gnus-html-image-id url))
+	      (push url urls)
+	      (push (gnus-html-image-id url) urls)
+	      (push "-o" urls)))))
+      (apply 'start-process 
+	     "images" nil "curl"
+	     "-s" "--create-dirs"
+	     urls))))
+
 ;;; gnus-html.el ends here
