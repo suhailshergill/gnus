606719a0229fcb57f36a36c609c547520c2bad5e
Lars Magne Ingebrigtsen <larsi@quimbies.gnus.org>
2010-11-24 21:16:58 +0100

    Revert the <#secure change, since it made yanking functions not work.

--- lisp/message.el
+++ lisp/message.el
@@ -3099,8 +3099,7 @@ M-RET    `message-newline-and-reformat' (break the line and reformat)."
     (expand-abbrev))
   (goto-char (point-min))
   (or (search-forward (concat "\n" mail-header-separator "\n") nil t)
-      (search-forward-regexp "[^:]+:\\([^\n]\\|\n[ \t]\\)+\n\n" nil t))
-  (while (looking-at "^<#secure") (forward-line 1)))
+      (search-forward-regexp "[^:]+:\\([^\n]\\|\n[ \t]\\)+\n\n" nil t)))
 
 (defun message-in-body-p ()
   "Return t if point is in the message body."
