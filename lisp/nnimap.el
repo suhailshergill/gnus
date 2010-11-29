a7a233dc43ffb3aa025f0fcf78cefd43694808a9
Lars Magne Ingebrigtsen <larsi@quimbies.gnus.org>
2010-11-29 05:31:24 +0100

    w32 open-network-stream doesn't seem to accept strings-with-numbers as port numbers,
    Author: Binjo <binjo.cn@gmail.com>

--- lisp/nnimap.el
+++ lisp/nnimap.el
@@ -348,7 +348,7 @@ textual parts.")
 	   (proto-stream-always-use-starttls t)
 	   connection-result login-result credentials)
       (when nnimap-server-port
-	(setq ports (append ports (list (format "%s" nnimap-server-port)))))
+	(setq ports (append ports (list nnimap-server-port))))
       (destructuring-bind (stream greeting capabilities)
 	  (open-protocol-stream
 	   "*nnimap*" (current-buffer) nnimap-address (car (last ports))
