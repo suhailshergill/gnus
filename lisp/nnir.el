suhailshergill/gnus

commit e1889675b7f4adf057833c5513c9374134c4e053

Use default value from global variable.
Without this the default search engine parameters aren't used at all.

lisp/nnir.el
@@ -1640,6 +1640,7 @@ server is of form 'backend:name'."
   (let ((method (gnus-server-to-method server)))
     (cond ((and method (assq key (cddr method)))
     	   (nth 1 (assq key (cddr method))))
+	  ((boundp key) (symbol-value key))
     	  (t nil))))
 
 (defun nnir-possibly-change-server (server)

commit 05cf0eae7ca3d7ac78a6423b89d395c5a2e7ff4b

* nnir.el (nnir-read-server-parm): Add an argument to restrict to 	server-variables only. This should fix a bug introduced with 	commit e1889675b7f4adf057833c5513c9374134c4e053.

lisp/nnir.el
@@ -1624,7 +1624,7 @@ actually)."
 	      (let* ((server (car x))
 		     (nnir-search-engine
 		      (or (nnir-read-server-parm 'nnir-search-engine
-						 server)
+						 server t)
 			  (cdr (assoc (car
 				       (gnus-server-to-method server))
 				      nnir-method-default-engines))))
@@ -1643,14 +1643,16 @@ actually)."
 		  nil)))
 	    groups))))
 
-(defun nnir-read-server-parm (key server)
-  "Returns the parameter value of key for the given server, where
-server is of form 'backend:name'."
+(defun nnir-read-server-parm (key server &optional not-global)
+  "Returns the parameter value corresponding to `key' for
+`server'. If no server-specific value is found consult the global
+environment unless `not-global' is non-nil."
   (let ((method (gnus-server-to-method server)))
     (cond ((and method (assq key (cddr method)))
-    	   (nth 1 (assq key (cddr method))))
-	  ((boundp key) (symbol-value key))
-    	  (t nil))))
+           (nth 1 (assq key (cddr method))))
+          ((and (not not-global) (boundp key)) (symbol-value key))
+          (t nil))))
+
 
 (defun nnir-possibly-change-server (server)
   (unless (and server (nnir-server-opened server))