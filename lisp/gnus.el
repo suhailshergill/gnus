80d445fda3dbded5239eaed46c7db8d877ef4308
Lars Ingebrigtsen <larsi@gnus.org>
2012-01-31 17:38:10 +0100

    No Gnus v0.20 is begun

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -293,7 +293,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "0.19"
+(defconst gnus-version-number "0.20"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "No Gnus v%s" gnus-version-number)
