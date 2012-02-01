0f6fbf486c943528c6f04f431c023d3922ab110c
Lars Ingebrigtsen <larsi@gnus.org>
2012-02-01 01:28:28 +0100

    * gnus.el (gnus-logo-color-alist): Change the colours for Ma Gnus.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -1008,10 +1008,11 @@ be set in `.emacs' instead."
     (purp "#9999cc" "#666699")
     (no "#ff0000" "#ffff00")
     (neutral "#b4b4b4" "#878787")
+    (ma "#2020e0" "#8080ff")
     (september "#bf9900" "#ffcc00"))
   "Color alist used for the Gnus logo.")
 
-(defcustom gnus-logo-color-style 'no
+(defcustom gnus-logo-color-style 'ma
   "*Color styles used for the Gnus logo."
   :type `(choice ,@(mapcar (lambda (elem) (list 'const (car elem)))
 			   gnus-logo-color-alist))
